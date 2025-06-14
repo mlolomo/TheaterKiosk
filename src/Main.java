a30bb933e7e364206999ed609cb2433c1a164949
Mbathio Lo <lombathio356@gmail.com>
2025-06-13 23:10:34 -0400

    Task 4 Completed

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //class Theater
+        //main()
+        //num age
+        //output "Enter your age: "
+        //input age
+        //if age >= 21 then
+        //   output "You get a paper wristband"
+        //end if
+        //return
+        //end class
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int age = 0;
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+
+        if (in.hasNextInt()) {
+            age = in.nextInt();
+            in.nextLine();
+
+            if (age >= 21) {
+                System.out.println("You get a paper wristband");
+            }
+
+        }
+        else {
+            String trash = in.nextLine();
+            System.out.println("Invalid input: " + trash);
         }
     }
 }
